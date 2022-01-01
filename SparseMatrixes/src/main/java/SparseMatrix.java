ae97ce0a58baee7dbd67c9598d9e4179dce94512
romanova <romanova@fom.team>
2022-01-02 02:17:37 +0300

    добавлена упаковка и распаковка разреженных матриц

--- SparseMatrixes/src/main/java/SparseMatrix.java
+++ SparseMatrixes/src/main/java/SparseMatrix.java
@@ -1,16 +1,31 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+@Getter @Setter
 public class SparseMatrix {
+    List<Integer> an;
+    int[] d;
+    int notNullEl;
+    int size;
+    int[][]sMatrix;
+    int[][] unpakagingMatrix;
 
-    public static void packaging(int[][]sMatrix){
-        List<Integer> an = new ArrayList<>();
-        int[] d = new int[sMatrix.length];
-        int notNullEl = 0;
-        int n = sMatrix.length;
-       // int m = sMatrix[0].length;
-        for (int i = 0; i < n; i++) {
+    public SparseMatrix(int[][]sMatrix){
+        this.sMatrix = sMatrix;
+    }
+
+    public void packaging(){
+        an = new ArrayList<>();
+        d = new int[sMatrix.length];
+        notNullEl = 0;
+        size = sMatrix.length;
+        for (int i = 0; i < size; i++) {
+            int flag = 0;
             for (int j = 0; j <=i; j++) {
                 if(i==j){
                    an.add(sMatrix[i][j]);
@@ -18,9 +33,15 @@ public class SparseMatrix {
                    notNullEl++;
                 }else{
                     if(sMatrix[i][j]>0){
+                        flag=1;
                         an.add(sMatrix[i][j]);
                         notNullEl++;
+                    }else if(flag==1){
+                        an.add(sMatrix[i][j]);
+                        notNullEl++;
+
                     }
+
 //                    else{
 ////                        if(sMatrix[i][j-1]>0&&sMatrix[i][j+1]>0){
 ////                            an.add(sMatrix[i][j]);
@@ -30,11 +51,27 @@ public class SparseMatrix {
                 }
             }
         }
-        an.stream().forEach(x-> System.out.printf(x+ ""));
+        an.forEach(x-> System.out.print(x+ " "));
         System.out.println();
-        Arrays.asList(d).stream().forEach(x-> System.out.printf(x+ ""));
+        Stream.of(d).forEach(x-> System.out.print(Arrays.toString(x) + ""));
         System.out.println();
         System.out.println(notNullEl);
 
+    }
+
+
+    public void unpackaging(){
+        unpakagingMatrix = new int[size][size];
+        unpakagingMatrix[0][0] = an.get(0);
+        for (int i = 1; i < size; i++) {
+               int j = d[i];
+               int k = i;
+               while (j> d[i-1]){
+                   unpakagingMatrix[i][k] = unpakagingMatrix[k][i] = an.get(j);
+                   j--;
+                   k--;
+               }
+        }
+        PrinterWord.printMatrix(unpakagingMatrix);
     }
 }
--- SparseMatrixes/src/main/java/SparseOut.java
+++ SparseMatrixes/src/main/java/SparseOut.java
@@ -1,6 +1,9 @@
 public class SparseOut {
     public static void main(String[] args) {
         int[][] out = { {1, 0, 0, 0, 0}, {0, 2, 6, 7, 0}, {0, 6, 3, 0, 0}, {0, 7, 0, 4, 8}, {0, 0, 0, 8, 5} };
-        SparseMatrix.packaging(out);
+         SparseMatrix sparseMatrix = new SparseMatrix(out);
+         sparseMatrix.packaging();
+         sparseMatrix.setSize(5);
+         sparseMatrix.unpackaging();
     }
 }
